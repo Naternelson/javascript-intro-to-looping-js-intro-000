050ac785c6a33154f4eba56e09489dd2b281e8d4
Nathan <7nater.nelson@gmail.com>
2020-01-16 19:09:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
+    if (i=0) { c
+      onsole.log("I am ${i} strange loops.")
+    }else{
     console.log("I am ${i} strange loops.")
   }
+}
   return array;
 }
